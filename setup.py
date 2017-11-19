dede94c33d0d062e16acbeb49bc4d7314921ce2f
Miloslav Pavelka <pavelkamiloslav@gmail.com>
2017-11-19 17:19:02 +0100

    Still renaming.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name='token',
+setup(name='timetoken',
       version='0.0.1',
       description='Universal Token API for generating and validating time limited access tokens.',
-      url='https://github.com/mpavelka/python-token',
+      url='https://github.com/mpavelka/python-timetoken',
       author='Miloslav Pavelka',
       author_email='pavelkamiloslav@gmail.com',
       license='BSD-3',
