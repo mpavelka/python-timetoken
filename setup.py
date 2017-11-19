26b8050e545a909aca4a2838097241e59655c055
Miloslav Pavelka <pavelkamiloslav@gmail.com>
2017-11-19 14:46:46 +0100

    Adding source and setup script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='token',
+      version='0.0.1',
+      description='Universal Token API for generating and validating access tokens.',
+      url='https://github.com/mpavelka/python-token',
+      author='Miloslav Pavelka',
+      author_email='pavelkamiloslav@gmail.com',
+      license='BSD-3',
+      packages=find_packages(),
+      zip_safe=False)
